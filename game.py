c42cab37b93dec8a0b88643b66d655daaf4a48a0
cyntain <oliver.harvey1@gmail.com>
2014-06-17 20:09:39 +0100

    [Cyntain] Added somemore things

--- game.py
+++ game.py
@@ -10,20 +10,19 @@ TILESIZE = 30
 
 # Tiles
 GRASS = 0
-GRASS_BOT_RIGHT = 1
-GRASS_BOT_LEFT = 2
-GRASS_TOP_RIGHT = 3
-GRASS_TOP_LEFT = 4
-WATER = 5
-STATION = 6
+WATER = 1
+STATION = 2
 
 score = 0
+ranY = random.randint(0,MAPHEIGHT)
+ranX = random.randint(0,MAPWIDTH)
 
 def main():
-    
-    def updateScore(add, amount):
-        """ Add or take amount from the players score, need to add different check to update the score
-            add = True then add amount or add = Flase take amount
+    def updateScore(action, amount):
+        """
+           - Add or take amount from the players score, need to add different check to update the score
+           - action = True then add amount or action = Flase take amount
+           - action True means good False means bad
         """
         global score
         if add == True:
@@ -37,6 +36,28 @@ def main():
             return score
         return score
     
+    def tileSelector(currentTile):
+        """
+            - Takes the argument of currentTile and checks what that tile is and preforms an action according to that tile
+        """
+        if currentTile == STATION:
+            print("THIS IS THE STATION")
+        elif currentTile == GRASS:
+            print("THIS IS GRASS")
+        elif currentTile == WATER:
+            print("THIS IS WATER")
+        else:
+            print("Unknown Tile! ERROR: ", currentTile)
+
+    def selectTile(counter):
+        """ If the counter is even then it means the tile is selected else is is unselected
+        """
+        mouseClicked = counter % 2
+        if mouseClicked == 0:
+            return True
+        else:
+            return False
+            
     pygame.init()
     # Set the size of the screen
     DISPLAY = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE + 50))   # Set the size of the screen according to the mapwidth and height times by tile size then add 50 to add the blackstrip at the bottom
@@ -49,97 +70,99 @@ def main():
     cursorPos = [0, 0]                                                                                                              # List of the cursor coord x, y
     mousex = 0                                                                                                                      # x coord of the mouse
     mousey = 0                                                                                                                      # y coord of the mouse
-    mouseClicked = False                                                                                                     # var to check state of the mouse button
+    mouseClicked = 1
     
-    # Images for the tile map
+    # Images for the game
     textures = {
                             GRASS : pygame.image.load("grass.png"),
-                            GRASS_BOT_RIGHT : pygame.image.load("grass_bot_right.png"),
-                            GRASS_BOT_LEFT : pygame.image.load("grass_bot_left.png"),
-                            GRASS_TOP_RIGHT : pygame.image.load("grass_top_right.png"),
-                            GRASS_TOP_LEFT : pygame.image.load("grass_top_left.png"),
                             WATER : pygame.image.load("water.png"),
                             STATION : pygame.image.load("station.png")
                             }
     
-    # List of all resources that can be generated
-    resources = [GRASS, GRASS_BOT_RIGHT, GRASS_BOT_LEFT, GRASS_TOP_RIGHT, GRASS_TOP_LEFT, WATER, STATION]
-    tilemap = [[GRASS for w in range(MAPWIDTH + 1)] for h in range(MAPHEIGHT + 1)] # Create the tile map with just GRASS in it
+    resources = [GRASS, WATER, STATION]                                                                 # List of all resources that can be generated
+    resourceName = ["Grass", "Water", "Station"]                                                            # Lists all of the resources names
+    tilemap = [[GRASS for w in range(MAPWIDTH + 1)] for h in range(MAPHEIGHT + 1)]    # Create the tile map with just GRASS in it
                                
-    for rw in range(MAPHEIGHT): # Check through the y axis
-        for cl in range(MAPWIDTH): # Check through the x axis
-            ranNum = random.randint(0,15) # generate a random number
-            if ranNum <= 10:
+    for rw in range(MAPHEIGHT):                                              # Check through the y axis
+        for cl in range(MAPWIDTH):                                            # Check through the x axis
+            ranNum = random.randint(0,15)                                   # generate a random number
+            if ranNum <= 10:                                                        # There is a 10 in 15 chance that the tile generated will be grass
                 tile = GRASS
             else:
                 tile = WATER
-                tilemap[rw][cl] = tile # add that tile to the tilemap at y, x
-    tilemap[random.randint(0,MAPHEIGHT)][random.randint(0,MAPWIDTH)] = STATION # create one station in the map
+            tilemap[rw][cl] = tile                                                       # add that tile to the tilemap at y, x
+    tilemap[ranY][ranX] = STATION                                               # create one station in the map
 
     while True:
-        for event in pygame.event.get():   # get all user inputs
-            if event.type == QUIT: # check if the input is exit 
-                pygame.quit() # close the game engine
-                sys.exit() # close the window
+        for event in pygame.event.get():                                            # get all user inputs
+            if event.type == QUIT:                                                     # check if the input is exit 
+                pygame.quit()                                                             # close the game engine
+                sys.exit()                                                                   # close the window
                 
-            #Player movement
+            ## Player movement
             elif event.type == KEYDOWN:
-                if event.key == K_RIGHT and playerPos[0] < MAPWIDTH - 1:
-                    playerPos[0] += 1
-                if event.key == K_LEFT and playerPos[0] > 0:
-                    playerPos[0] -= 1
-                if event.key == K_UP and playerPos[1] > 0:
-                    playerPos[1] -= 1
-                if event.key == K_DOWN and playerPos[1] < MAPHEIGHT - 1:
-                    playerPos[1] += 1
+                if event.key == K_RIGHT and playerPos[0] < MAPWIDTH - 1:                # Binds the player within the game screen
+                    playerPos[0] += 1                                                                           # Moves the player by one tile
+                if event.key == K_LEFT and playerPos[0] > 0:                                       # Binds the player within the game screen
+                    playerPos[0] -= 1                                                                            # Moves the player by one tile
+                if event.key == K_UP and playerPos[1] > 0:                                          # Binds the player within the game screen
+                    playerPos[1] -= 1                                                                            # Moves the player by one tile
+                if event.key == K_DOWN and playerPos[1] < MAPHEIGHT - 1:              # Binds the player within the game screen
+                    playerPos[1] += 1                                                                           # Moves the player by one tile
                     
-            # Mouse position 
-            elif event.type == MOUSEMOTION:
-                mousex, mousey = event.pos
-                mousex = int(mousex/TILESIZE)
-                mousey = int(mousey/TILESIZE)
-                
-                if mousex >0 and mousex <= MAPWIDTH*TILESIZE:
-                    cursorPos[0] = mousex
-                else:
-                    cursorPos[0] = 0
-                if mousey > 0 and mousey <= MAPHEIGHT*TILESIZE:
-                    cursorPos[1] = mousey
-                else:
-                    cursorPos[1] = 0
-                
+             ## Mouse buttons
             elif event.type == MOUSEBUTTONUP:
-                mousex, mousey = event.pos
-                mousex = int(mousex/TILESIZE)
-                mousey = int(mousey/TILESIZE)
-                currentTile = tilemap[cursorPos[1]][cursorPos[0]]
-                print(currentTile)
-
-                if mousex >0 and mousex <= MAPWIDTH*TILESIZE:
-                    cursorPos[0] = mousex
+                mousex, mousey = event.pos                                                                   # Sets the mouse pos into mousex and mousey
+                mousex = int(mousex/TILESIZE)                                                               # converts the mousex into tile size
+                mousey = int(mousey/TILESIZE)                                                               # converts the mousey into tile size
+                currentTile = tilemap[cursorPos[1]][cursorPos[0]]                                       # Saves the tile that is clicked into this var
+                if mousex > 0 and mousex <= MAPWIDTH:                                   # checks if the mouse pos is within the screen on the x axis
+                    cursorPos[0] = mousex                                                                          # sets the mouse x into the list of cursorPos
                 else:
-                    cursorPos[0] = 0
-                if mousey > 0 and mousey <= MAPHEIGHT*TILESIZE:
-                    cursorPos[1] = mousey
+                    cursorPos[0] = 0                                                                                    # if the mouse pos if out side of the boundaries then set the cursor pos to 0
+                if mousey > 0 and mousey <= (MAPHEIGHT - 1):                             # checks if the mouse pos is within the screen on the y axis
+                    cursorPos[1] = mousey                                                                           # sets the mouse y into the list of cursorPos
                 else:
-                    cursorPos[1] = 0
-                mouseClicked = True
+                    cursorPos[1] = 0                                                                                    # if the mouse is out side of the boundaries then set the cursor pos to 0
+                mouseClicked += 1
+                tileSelector(currentTile)                                                                               # Checks what the tile is and preforms an action according to that tile
 
-        # Places tiles on to the screen   
-        for row in range(MAPHEIGHT ):
-            for column in range(MAPWIDTH):
-                DISPLAY.blit(textures[tilemap[row][column]], (column*TILESIZE, row*TILESIZE))
                 
-          # Draws the player on to the screen  
-        DISPLAY.blit(PLAYER, (playerPos[0]*TILESIZE, playerPos[1]*TILESIZE))
-        DISPLAY.blit(CURSOR, (cursorPos[0]*TILESIZE, cursorPos[1]*TILESIZE))
+                
+            ## Mouse control
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos                                                               # Saves the Pos of the mouse in mousex and mousey
+                mousex = int(mousex/TILESIZE)                                                           # converts the mousex into tile size
+                mousey = int(mousey/TILESIZE)                                                           # converts the mousey into tile size
+                tileChosen = selectTile(mouseClicked)
+                
+                if tileChosen == False:
+                    if mousex > 0 and mousex <= MAPWIDTH :                             # checks if the mouse pos is within the screen on the x axis
+                        cursorPos[0] = mousex                                                                    # sets the mouse x into the list of cursorPos
+                    else:
+                        cursorPos[0] = 0                                                                                # if the mouse pos if out side of the boundaries then set the cursor pos to 0
+                    if mousey > 0 and mousey <= (MAPHEIGHT - 1) :                             # checks if the mouse pos is within the screen on the y axis
+                        cursorPos[1] = mousey                                                                      # sets the mouse y into the list of cursorPos
+                    else:
+                        cursorPos[1] = 0                                                                                # if the mouse is out side of the boundaries then set the cursor pos to 0
+                    
+
+
+        ## Places tiles on to the screen   
+        for row in range(MAPHEIGHT ):                                                                                     # tiles on the y axis
+            for column in range(MAPWIDTH):                                                                              # tiles on the x axis
+                DISPLAY.blit(textures[tilemap[row][column]], (column*TILESIZE, row*TILESIZE))      # draw all of the possible tiles onto the screen
+                
+          
+        DISPLAY.blit(PLAYER, (playerPos[0]*TILESIZE, playerPos[1]*TILESIZE))                          # Draws the player on to the screen  
+        DISPLAY.blit(CURSOR, (cursorPos[0]*TILESIZE, cursorPos[1]*TILESIZE))                        # Draws the cursor on to the screen
         
         # Draws text on to the screen
-        textobj = FONT.render("SCORE: " + str(score), True, WHITE, BLACK)
-        DISPLAY.blit(textobj, (5, MAPHEIGHT*TILESIZE + 20))
+        textScore = FONT.render("SCORE: " + str(score), True, WHITE, BLACK)                      # Creates a textScore to be prep'ed to drawn onto the screen
+        DISPLAY.blit(textScore, (5, MAPHEIGHT*TILESIZE + 20))                                           # draws the textScore onto the screen
 
-        pygame.display.update() # update the screen for all the drawings
+        pygame.display.update()                                                                                         # update the screen for all the drawings
         
-if __name__ == '__main__':
+if __name__ == '__main__':                                                                                               # Lanches the main funtion
     main()
                 
